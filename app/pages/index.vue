ecc5753bbcd8ad8e98142ffe13bc64610147096a
Zeyad <zeyad0z>
2025-11-18 10:47:39 +0200

    update the home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -80,7 +80,7 @@ const prevItem = () => {
 
 <template>
   <div class="flex flex-col gap-4 w-full">
-    <!-- first -->
+    <!-- head -->
     <div
       class="border border-[#E9E9E9] flex justify-between bg-[#F9F9F9] py-5 px-4 rounded-[1.25rem]"
     >
@@ -129,45 +129,94 @@ const prevItem = () => {
         </button>
       </div>
     </div>
+    
+    <!-- main -->
+    <div class="flex gap-5">
+      <!-- right side -->
+      <div class="flex-2 flex flex-col gap-4">
+        <BigCard label="جديد المقالات" viewAll="true">
+          <Card :items="blogs" author="true" />
+        </BigCard>
 
-    <!-- new blogs + books -->
-    <div class="flex gap-5 w-full items-start">
-      <BigCard label="جديد المقالات" viewAll="true" class="w-[67.5%]">
-        <Card :items="blogs" author="true" />
-      </BigCard>
+        <BigCard label="جديد الفتاوي" viewAll="true">
+          <Card :items="blogs" author="true" />
+        </BigCard>
 
-      <BigCard label="الكتب" viewAll="true" class="w-[32.5%]">
-        <BooksCard :items="books" image="true" />
-      </BigCard>
-    </div>
+        <BigCard label="ارسال فتوي">
+          <form action="" class="m-4 flex flex-col gap-4">
+            <input
+              type="text"
+              class="w-full h-[2.5rem] bg-[#FBFBFB] rounded-3xl px-4 border border-[#EDEDED] text-[#133349]"
+              placeholder="الاسم"
+            />
+            <div class="grid grid-cols-2 gap-x-8 gap-y-4">
+              <input
+                type="text"
+                class="w-full h-[2.5rem] bg-[#FBFBFB] rounded-3xl px-4 border border-[#EDEDED] text-[#133349]"
+                placeholder="البريد الإلكترونى"
+              />
+              <input
+                type="text"
+                class="w-full h-[2.5rem] bg-[#FBFBFB] rounded-3xl px-4 border border-[#EDEDED] text-[#133349]"
+                placeholder="السن"
+              />
+              <input
+                type="text"
+                class="w-full h-[2.5rem] bg-[#FBFBFB] rounded-3xl px-4 border border-[#EDEDED] text-[#133349]"
+                placeholder="البلد"
+              />
+              <input
+                type="text"
+                class="w-full h-[2.5rem] bg-[#FBFBFB] rounded-3xl px-4 border border-[#EDEDED] text-[#133349]"
+                placeholder="موضوع الاستفتاء"
+              />
+            </div>
+            <textarea
+              class="w-full h-[6rem] bg-[#FBFBFB] rounded-3xl px-4 border border-[#EDEDED] text-[#133349] text-[0.87rem]"
+              placeholder="السؤال"
+            ></textarea>
+            <button
+              class="h-[3.5rem] w-fit px-7 bg-[#6B9A8A] rounded-4xl flex justify-center items-center gap-1 cursor-pointer"
+            >
+              <client-only>
+                <VsxIcon
+                  iconName="Edit2"
+                  :size="18"
+                  color="#ffffff"
+                  type="linear"
+                />
+              </client-only>
+              <p class="text-[0.87rem] text-[#ffffff]">ارسل فتواك</p>
+            </button>
+          </form>
+        </BigCard>
+      </div>
 
-    <!-- images -->
-    <div class="flex gap-4 absolute inset-0 top-162 right-284">
-      <img
-        src="../assets/right.png"
-        alt="book image"
-        class="w-[165px] h-[165px] object-cover rounded-2xl"
-      />
-      <img
-        src="../assets/left.png"
-        alt="book image"
-        class="w-[165px] h-[165px] object-cover rounded-2xl"
-      />
-    </div>
+      <!-- left side -->
+      <div class="flex flex-col gap-4">
+        <BigCard label="الكتب" viewAll="true" class="">
+          <BooksCard :items="books" image="true" />
+        </BigCard>
 
-    <div class="flex gap-5 w-full items-start relative">
-      <!-- جديد الفتاوي -->
-      <BigCard label="جديد الفتاوي" viewAll="true" class="w-[66%]">
-        <Card :items="blogs" author="true" />
-      </BigCard>
+        <!-- images -->
+        <div class="flex gap-4 mb-2">
+          <img
+            src="../assets/right.png"
+            alt="book image"
+            class="w-[165px] h-[165px] object-cover rounded-2xl"
+          />
+          <img
+            src="../assets/left.png"
+            alt="book image"
+            class="w-[165px] h-[165px] object-cover rounded-2xl"
+          />
+        </div>
 
-      <!-- المكتبة المقروءة -->
-      <div class="relative w-[32.5%]">
         <BigCard
           label="المكتبة المقروءة"
           blue="true"
           viewAll="true"
-          class="absolute top-40 w-full"
+          class="w-full"
         >
           <div class="grid grid-cols-3 grid-rows-3 gap-3 my-3 mx-2">
             <div
@@ -189,62 +238,8 @@ const prevItem = () => {
             </div>
           </div>
         </BigCard>
-      </div>
-    </div>
 
-    <!-- form + eamil -->
-    <div class="flex gap-5">
-      <BigCard label="ارسال فتوي" class="w-[66%]">
-        <form action="" class="m-4 flex flex-col gap-4">
-          <input
-            type="text"
-            class="w-full h-[2.5rem] bg-[#FBFBFB] rounded-3xl px-4 border border-[#EDEDED] text-[#133349]"
-            placeholder="الاسم"
-          />
-          <div class="grid grid-cols-2 gap-x-8 gap-y-4">
-            <input
-              type="text"
-              class="w-full h-[2.5rem] bg-[#FBFBFB] rounded-3xl px-4 border border-[#EDEDED] text-[#133349]"
-              placeholder="البريد الإلكترونى"
-            />
-            <input
-              type="text"
-              class="w-full h-[2.5rem] bg-[#FBFBFB] rounded-3xl px-4 border border-[#EDEDED] text-[#133349]"
-              placeholder="السن"
-            />
-            <input
-              type="text"
-              class="w-full h-[2.5rem] bg-[#FBFBFB] rounded-3xl px-4 border border-[#EDEDED] text-[#133349]"
-              placeholder="البلد"
-            />
-            <input
-              type="text"
-              class="w-full h-[2.5rem] bg-[#FBFBFB] rounded-3xl px-4 border border-[#EDEDED] text-[#133349]"
-              placeholder="موضوع الاستفتاء"
-            />
-          </div>
-          <textarea
-            class="w-full h-[6rem] bg-[#FBFBFB] rounded-3xl px-4 border border-[#EDEDED] text-[#133349] text-[0.87rem]"
-            placeholder="السؤال"
-          ></textarea>
-          <button
-            class="h-[3.5rem] w-fit px-7 bg-[#6B9A8A] rounded-4xl flex justify-center items-center gap-1 cursor-pointer"
-          >
-            <client-only>
-              <VsxIcon
-                iconName="Edit2"
-                :size="18"
-                color="#ffffff"
-                type="linear"
-              />
-            </client-only>
-            <p class="text-[0.87rem] text-[#ffffff]">ارسل فتواك</p>
-          </button>
-        </form>
-      </BigCard>
-
-      <div class="relative w-[32.5%]">
-        <BigCard label="النشرة البريدية" class="absolute top-26 w-full">
+        <BigCard label="النشرة البريدية" class="w-full">
           <div
             class="flex flex-col justify-center items-center gap-4 py-14 px-3"
           >
